8b5935ab309954fbad1bb077d8f35df4407df550
Pavel Aleksanochkin <soker.96@mail.ru>
2025-05-07 12:36:08 +0300

    added hw4 , hw6

---
+++ src/test/java/hw6/utils/FakeData.java
@@ -0,0 +1,77 @@
+
+package hw6.utils;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class FakeData {
+
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    // Методы для получения случайных данных
+    public static String getRandomFirstName() {
+        return faker.name().firstName();
+    }
+
+    public static String getRandomLastName() {
+        return faker.name().lastName();
+    }
+
+    public static String getRandomEmail() {
+        return faker.internet().emailAddress();
+    }
+
+    public static String getRandomGender() {
+        return faker.options().option("Male", "Female", "Other");
+    }
+
+    public static String getRandomPhoneNumber() {
+        return faker.phoneNumber().subscriberNumber(10);
+    }
+
+    public static String getRandomDayOfBirth() {
+        return String.format("%02d", faker.number().numberBetween(1, 28));
+    }
+
+    public static String getRandomMonthOfBirth() {
+        return faker.options().option(
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+        );
+    }
+
+    public static String getRandomYearOfBirth() {
+        return String.valueOf(faker.number().numberBetween(1970, 2024));
+    }
+
+    public static String getRandomSubject() {
+        return faker.options().option("Physics", "Computer Science", "Chemistry", "Economics");
+    }
+
+    public static String getRandomHobbies() {
+        return faker.options().option("Sports", "Reading", "Music");
+    }
+
+    public static String getRandomPicture() {
+        return "Исх. № ФС-3.25 от 22.01.pdf"; // Или можно оставить фиктивную опцию
+    }
+
+    public static String getRandomAddress() {
+        return faker.address().fullAddress();
+    }
+
+    public static String getRandomState() {
+        return faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+    }
+
+    public static String getRandomCity(String state) {
+        return switch (state) {
+            case "NCR" -> faker.options().option("Delhi", "Gurgaon", "Noida");
+            case "Uttar Pradesh" -> faker.options().option("Agra", "Lucknow", "Merrut");
+            case "Haryana" -> faker.options().option("Karnal", "Panipat");
+            case "Rajasthan" -> faker.options().option("Jaipur", "Jaiselmer");
+            default -> null;
+        };
+    }
+}
